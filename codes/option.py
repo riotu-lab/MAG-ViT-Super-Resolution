2431e9a6d942f4b1bd25d78028207d6b2c4ed381
Anas M. Ali <65374131+AnasHXH@users.noreply.github.com>
2026-08-20 14:43:42 +0300

    Update option.py

--- codes/option.py
+++ codes/option.py
@@ -77,6 +77,8 @@ parser.add_argument('--precision', type=str, default='single',
 # training specifications
 parser.add_argument('--reset', action='store_true', default=False,
                     help='reset the training')
+
+
 parser.add_argument('--epochs', type=int, default=500,
                     help='number of epochs to train')
 parser.add_argument('--batch_size', type=int, default=16,
@@ -93,7 +95,7 @@ parser.add_argument('--test_metric', type=str, default='psnr',
                     help='for best model selection in test phase (psnr, ssim)')
 
 # optimization specifications
-parser.add_argument('--lr', type=float, default=1e-4,
+parser.add_argument('--lr', type=float, default=4e-4,
                     help='learning rate')
 parser.add_argument('--lr_decay', type=int, default=400,
                     help='learning rate decay per N epochs')
